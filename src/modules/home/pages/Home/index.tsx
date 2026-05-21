988ae11099ea88d3aefc02d680538c3a05207bb0
Rodrigo Santos <rolns002@gmail.com>
2026-05-21 14:56:22 -0300

    feat: adicionar componente ProjectCard e integrar na seção de projetos da página inicial

--- src/modules/home/pages/Home/index.tsx
+++ src/modules/home/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import HomeHero from './components/Sections/Hero'
 import HomeJobs from './components/Sections/Jobs'
 import HomeSetup from './components/Sections/Setup'
 import HomeContact from './components/Sections/Contact'
-import HomeProjects from './components/Sections/Projects'
+import HomeProjects from './components/Sections/PersonalProjects'
 import HomeContributions from './components/Sections/Contributions'
 
 export default function Home() {
